6b8e86d0cc9cd114a1b47e5e8f253e53f0ae0fd4
jxhdev <jxhdev@gmail.com>
2018-07-24 09:41:28 -0700

    finishing up for submission

--- src/Joke.js
+++ src/Joke.js
@@ -4,9 +4,15 @@ export default class Joke extends Component {
   render() {
     return (
       <div>
-        <li>{this.props.text}</li>
-        <button>Upvote</button>
-        <button>Downvote</button>
+        <button onClick={() => this.props.upvote(this.props.id)}>Upvote</button>
+        <button onClick={() => this.props.downvote(this.props.id)}>
+          Downvote
+        </button>
+
+        <p style={{ display: 'inline-block', marginRight: '10px' }}>
+          {this.props.votes}
+        </p>
+        <li style={{ display: 'inline-block' }}>{this.props.text}</li>
       </div>
     );
   }
--- src/JokeList.js
+++ src/JokeList.js
@@ -4,69 +4,109 @@ import Joke from './Joke';
 import uuid from 'uuid/v1';
 import { PacmanLoader, CircleLoader, HashLoader } from 'react-spinners';
 import './JokeList.css';
-
+var numJokes = 20;
 export default class JokeList extends Component {
   constructor(props) {
     super();
     this.seenJokes = new Set();
     this.state = {
       jokes: [],
-      loading: true,
+      loading: false,
       loadingMessage: 'Loading...'
     };
   }
   getJokes = async () => {
-    while (this.state.jokes.length < 20) {
-      const res = await axios.get('https://icanhazdadjoke.com', {
-        headers: { Accept: 'application/json' }
-      });
-      if (!this.seenJokes.has(res.data.joke)) {
-        this.seenJokes.add(res.data.jokes);
-        this.setState({
-          jokes: [...this.state.jokes, res.data.joke]
+    try {
+      while (this.state.jokes.length < numJokes) {
+        const res = await axios.get('https://icanhazdadjoke.com', {
+          headers: { Accept: 'application/json' }
         });
-      } else {
-        console.log('duplicate found!');
+        if (!this.seenJokes.has(res.data.joke)) {
+          this.seenJokes.add(res.data.jokes);
+          this.setState({
+            jokes: [
+              ...this.state.jokes,
+              { joke: res.data.joke, id: uuid(), votes: 0 }
+            ]
+          });
+        } else {
+          console.log('duplicate found!');
+        }
       }
+      this.setState({
+        loading: false,
+        loadingMessage: ''
+      });
+    } catch (e) {
+      console.log(e);
     }
-    this.setState({
-      loading: false,
-      loadingMessage: ''
-    });
   };
   componentWillMount = async () => {
-    // try {
-    //   if (this.state.jokes.length < 20) {
-    //     while (this.state.jokes.length < 20) {
-    //       const res = await axios.get('https://icanhazdadjoke.com', {
-    //         headers: { Accept: 'application/json' }
-    //       });
-    //       if (!this.seenJokes.has)
-    //     }
-    //   }
-    // } catch (e) {
-    //   console.log('error', e);
-    // }
-    this.setState({ loading: true });
-    if (this.state.jokes.length === 20) {
-      this.setState(
-        {
-          jokes: [],
-          loading: true,
-          loadingMessage: 'Loading...'
-        },
-        this.getJokes
-      );
-    } else {
-      this.getJokes();
+    try {
+      this.setState({ loading: true });
+      if (this.state.jokes.length === numJokes) {
+        this.setState(
+          {
+            jokes: [],
+            loading: true,
+            loadingMessage: 'Loading...'
+          },
+          this.getJokes
+        );
+      } else {
+        this.getJokes();
+      }
+    } catch (e) {
+      console.log(e);
     }
+  };
+
+  sortJokes = () => {
+    const sortedJokes = [...this.state.jokes].sort((a, b) => a.votes < b.votes);
+    this.setState({
+      jokes: sortedJokes
+    });
+  };
+
+  handleUpvote = id => {
+    console.log('hey!');
+    this.setState({
+      jokes: this.state.jokes.map(joke => {
+        if (joke.id === id) {
+          joke.votes = joke.votes + 1;
+        }
+        return joke;
+      })
+    });
+    this.sortJokes();
+  };
+
+  handleDownvote = id => {
+    this.setState({
+      jokes: this.state.jokes.map(joke => {
+        if (joke.id === id) {
+          joke.votes = joke.votes - 1;
+        }
+        return joke;
+      })
+    });
+    this.sortJokes();
   };
   render() {
     if (!this.state.loading) {
       var jokes = (
         <ul>
           {this.state.jokes.map(j => {
-            return <Joke text={j} key={uuid()} />;
+            return (
+              <Joke
+                text={j.joke}
+                id={j.id}
+                key={j.id}
+                votes={j.votes}
+                upvote={this.handleUpvote}
+                downvote={this.handleDownvote}
+              />
+            );
           })}
         </ul>
       );
